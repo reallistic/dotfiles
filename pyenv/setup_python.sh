84dfb5d6bd91a2d47b1debab8e52791f0d81829a
Michael Chase <3686226+reallistic@users.noreply.github.com>
2023-01-22 12:41:57 -0800

    fix python requirements symlinks

--- pyenv/setup_python.sh
+++ pyenv/setup_python.sh
@@ -17,7 +17,7 @@ PY_VERSIONS=(
 )
 
 INSTALL_LOCATION="$HOME/.pyenv"
-REQS_LOCATION="$HOME/.pyenv/reqs"
+REQS_LOCATION="$HOME/.config/python_requirements"
 DEFAULT_REQUIREMENTS_IN_FILE="$REQS_LOCATION/global_requirements.in"
 
 if [ ! -f $DEFAULT_REQUIREMENTS_IN_FILE ]; then
