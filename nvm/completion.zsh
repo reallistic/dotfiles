9e0d8c6abeb084d4f253cd5fe16fe89f1c5b12d1
Michael Chase <3686226+reallistic@users.noreply.github.com>
2024-10-24 18:41:53 -0700

    remove nvm

--- nvm/completion.zsh
+++
@@ -1,4 +0,0 @@
-# nvm bash_completion
-if [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ]; then
-    . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
-fi
--- nvm/install.zsh
+++
@@ -1,9 +0,0 @@
-export NVM_DIR="$HOME/.nvm"
-
-if [ -s "/usr/local/opt/nvm/nvm.sh" ]; then
-    . "/usr/local/opt/nvm/nvm.sh"
-fi
-
-if command -v metatron &> /dev/null; then
-    export NODE_PATH=`nvm which current`
-fi
